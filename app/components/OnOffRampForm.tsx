10907c24f6800d5b538de4520cf1cfd48db6a6ee
Sandile <ssiwundla023@student.wethinkcode.co.za>
2025-02-11 21:20:20 +0200

    Modal stablelisation, background coloring and progress bar

--- app/components/OnOffRampForm.tsx
+++ app/components/OnOffRampForm.tsx
@@ -18,23 +18,29 @@ const OnOffRampForm = () => {
 
   return (
     <>
-    <div className="on-off-ramp-form p-6 rounded-lg shadow-md bg-stone-600 w-auto max-h-screen mt-4">
-      {/* <h1 className="text-2xl font-bold mb-4 text-center">Universal</h1> */}
-      <div className='justify-center rounded-lg border '>
-        <p>{formData.action.toUpperCase()}</p>
+      <div
+        className="on-off-ramp-form p-6 rounded-lg shadow-md"
+        style={{
+          width: '750.47px',
+          height: '650px',
+          backgroundColor: '#68BEFF40', // Blue transparent background
+          border: '2px solid #039FFF',
+        }}
+      >
+        <div className="justify-center rounded-lg border">
+          <p>{formData.action.toUpperCase()}</p>
+        </div>
+        <div className="steps flex justify-center mb-4 w-full">
+          <StepIndicator currentStep={step} />
+        </div>
+        {step === 1 && <AmountStep onNext={handleNext} />}
+        {step === 2 && <WalletStep onNext={handleNext} onBack={handleBack} />}
+        {step === 3 && <VerifyStep onNext={handleNext} onBack={handleBack} />}
+        {step === 4 && <OrderStep onBack={handleBack} />}
+        <Button>BUY</Button>
+        <Button>SELL</Button>
+        <Button>TRANSFER</Button>
       </div>
-      <div className="steps flex justify-center mb-4 w-full">
-        <StepIndicator currentStep={step} />
-      </div>
-      {step === 1 && <AmountStep onNext={handleNext} />}
-      {step === 2 && <WalletStep onNext={handleNext} onBack={handleBack} />}
-      {step === 3 && <VerifyStep onNext={handleNext} onBack={handleBack} />}
-      {step === 4 && <OrderStep onBack={handleBack} />}
-      <Button >BUY</Button>
-      <Button >SELL</Button>
-      <Button >TRANSFER</Button>
-    </div>
-    
     </>
   );
 };
